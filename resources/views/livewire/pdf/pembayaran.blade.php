0d04e359e464a18cfbf70f9ebbe5cd94195df447
inaya mutmainna <inayamutmainna@MacBook-Air-inaya-mutmainna.local>
2025-07-21 09:03:58 +0800

    update

--- resources/views/livewire/pdf/pembayaran.blade.php
+++ resources/views/livewire/pdf/pembayaran.blade.php
@@ -3,68 +3,127 @@
 
 <head>
     <meta charset="utf-8">
-    <title>Pembayaran</title>
+    <title>Struk Pembayaran</title>
 
     <style>
         body {
             font-family: sans-serif;
-            font-size: 12px;
+            font-size: 15px;
         }
 
         table {
-            width: 100%;
+            width: 50%;
             border-collapse: collapse;
             margin-top: 20px;
         }
 
-        th,
+        .header {
+            text-align: center;
+        }
+
+        .ucapan {
+            position: relative;
+            text-align: center;
+            top: 200px;
+        }
+
+        th {
+            border: 1px solid #000;
+            padding: 15px;
+            left: 50px;
+            top: 50px;
+        }
+
         td {
             border: 1px solid #000;
-            padding: 5px;
+            padding: 15px;
             text-align: left;
+            left: -50px;
+            width: 90px;
+            top: 50px;
         }
 
         #home {
             width: 70px;
-            height: 70px;
+            height: 80px;
             position: relative;
-            top: -20px;
-            left: 650px;
+            top: -10px;
+            left: 0px;
 
 
         }
 
-        h2 {
-            top: -100px;
+
+        h3 {
+            top: -120px;
+            left: 30px;
             position: relative;
             text-align: center;
         }
+
+        .data {
+            position: relative;
+            top: -70px;
+            left: 20px;
+
+        }
+
+        h6 {
+            top: -140px;
+            left: 30px;
+            position: relative;
+            text-align: center;
+
+        }
+
+        .nama {
+            position: relative;
+            top: -60px;
+            left: 300px;
+        }
+
+        h4 {
+            position: relative;
+            top: -40px;
+            left: 20px;
+        }
+
+        #jats {
+            position: relative;
+            top: -5px;
+            left: 450px;
+        }
     </style>
+    <img id="home" src="image/smk.png" width="200" height="200">
     <img id="home" src="image/logo.png" width="200" height="200">
-    <h2>Kwitansi Pembayaran</h2>
+    <img id="jats" src="image/jats.png" width="100" height="100" left="100">
+    <h3>AUTO JATS SERVIS SMKN 1 ANGGANA</h3>
+    <h6> Jl. Handil Soppeng , RT. 10,Kutai Lama, Kec. Anggana, Kab.Kutai Kartanegara,<br> KalimantanTimur| Kode Pos75381</h6>
 
 </html>
 </head>
 
 <body>
+
     @php
-
     @endphp
-    <p><strong>Tanggal:</strong> {{ $pembayaran->tanggal_pembayaran }}</p>
-    <p><strong>Customer:</strong> {{ $pembayaran->costumer->nama_costumer }}</p>
-    <p><strong>Alamat:</strong> {{ $pembayaran->costumer->alamat_costumer }}</p>
-    <p><strong>No Hp:</strong> {{ $pembayaran->costumer->no_hp_costumer }}</p>
-    <p><strong>Email:</strong> {{ $pembayaran->costumer->email_costumer }}</p>
-
+    <div class="data">
+        <p><strong>Customer:</strong> {{ $pembayaran->costumer->nama_costumer }}</p>
+        <p><strong>Alamat:</strong> {{ $pembayaran->costumer->alamat_costumer }}</p>
+        <p><strong>No Hp:</strong> {{ $pembayaran->costumer->no_hp_costumer }}</p>
+        <p><strong>Email:</strong> {{ $pembayaran->costumer->email_costumer }}</p>
+        <p><strong>Tanggal:</strong> {{ $pembayaran->tanggal_pembayaran }}</p>
+    </div>
     <h4>Detail Pembayaran:</h4>
     <table>
         <thead>
-            <th>Nama Sparepart</th>
+            <th> Nama Sparepart</th>
             <th>Jumlah</th>
             <th>Harga</th>
             <th>Nama Service</th>
             <th>Jumlah</th>
             <th>Harga</th>
+
         </thead>
         <tbody>
             @foreach ($pembayaran->items as $item)
@@ -78,12 +137,16 @@
             </tr>
             @endforeach
         </tbody>
-
     </table>
 
     <p><strong>Total Harga:</strong> Rp{{ number_format($pembayaran->total_harga, 0, ',', '.') }}</p>
     <p><strong>Total Bayar:</strong> Rp{{ number_format($pembayaran->total_bayar, 0, ',', '.') }}</p>
     <p><strong>Kembalian:</strong> Rp{{ number_format($pembayaran->total_kembali, 0, ',', '.') }}</p>
+
+    <div class="ucapan">
+        Terima kasih telah menggunakan layanan kami!<br>
+        Semoga kendaraan Anda selalu dalam kondisi terbaik.
+    </div>
 </body>
 
 </html>
